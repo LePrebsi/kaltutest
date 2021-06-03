a326609dc93f13a721db58f0e61ae0a496ab578b
LePrebsi <85283611+LePrebsi@users.noreply.github.com>
2021-06-03 11:43:29 +0200

    Update Kaltutest.js

--- Kaltutest.js
+++ Kaltutest.js
@@ -1,4 +1,7 @@
 mw.setConfig(
-  'webkitSupportsFullscreen', false
-  'document.EnableFullscreen', true
+  doFullScreenPlayer: function( callback ) {
+    if( !this.fullScreenApiExcludes() && screenfull && screenfull.enabled(doc) && !mw.isOldAndroidChromeNativeBrowser() && !((mw.isIpad() || mw.isIpadOS()) && mw.getConfig('EmbedPlayer.EnableIpadNativeFullscreen'))) {
+				callFullScreenAPI();
+}  return true;
+  'document.EnableFullscreen', true;
 );
